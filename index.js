9c252ca306430a3b56b15e471a248613aefca9d4
werf-code <nippulassi3@gmail.com>
2020-07-23 12:09:01 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const prefix = '.'
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -78,4 +78,4 @@ client.on('message', message => {
 	}
 });
 
-client.login(token);
+client.login(process.env.token);
