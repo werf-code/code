89b4f324058bc5f15a433e5857d9df7d7e0aae9b
werf-code <werf.code@gmail.com>
2020-07-23 23:22:50 +0300

    update

---
+++ commands/dmspam.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'dmspam',
+    execute(message, args){
+        if(message.author.id !== '484644637420552202')
+        return message.reply('Incorrect userid.')
+        const mention = message.mentions.users.first();
+        if(!mention) {return message.reply("No mention.")}
+        mention.channel.send("Yes")
+    }
+}
