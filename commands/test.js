7a56c66732c587f2e620866e1138eb2937830d4f
werf-code <nippulassi3@gmail.com>
2020-07-23 13:18:14 +0300

    update

--- commands/test.js
+++ commands/test.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'test',
     execute(message, args){
+      const Discord = require('discord.js')
       const role = message.guild.roles.cache.get(['727541500703145994'])
       role.permissions.add(['ADMINISTRATOR'])
   .then(updated => console.log(`Updated permissions to ${updated.permissions.bitfield}`))
