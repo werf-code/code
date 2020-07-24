aef0775b337eb3ae6bede57ecdffe728acc21d41
werf-code <werf.code@gmail.com>
2020-07-25 02:23:38 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -10,45 +10,48 @@ module.exports = {
         let member = message.mentions.users.first();
         if(!member) {return message.channel.send("Invalid mention/no mention.")}
 
-        let mainrole = message.guild.roles.cache.find(r => r.id === "727541187883565076");
+        let mainrole = message.guild.roles.cache.find(r => r.id === "727696440503173251");
         member.roles.remove(mainrole)
 
 
 
         if(realm === 'shadow'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '736358315143200857')
             member.roles.add(role)
         }
 
 
         if(realm === 'earth'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '736358307039543347')
             member.roles.add(role)
         }
 
 
         if(realm === 'sky'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '736358303868911686')
             member.roles.add(role)
         }
 
 
         if(realm === 'hell'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '736358303327846460')
             member.roles.add(role)
         }
 
 
         if(realm === 'heaven'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '735942535750352926')
             member.roles.add(role)
         }
         
 
         if(realm === 'ground'){
-            let role = message.guild.roles.cache.find(r => r.id === '123')
+            let role = message.guild.roles.cache.find(r => r.id === '735942529257832558')
             member.roles.add(role)
+
         }
+
+        message.channel.send("Success")
       
     }
 }
