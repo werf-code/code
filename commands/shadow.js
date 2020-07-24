4e05acb5b099fc65da2530aef9eeea42540c3ac3
werf-code <werf.code@gmail.com>
2020-07-25 01:57:25 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -3,13 +3,16 @@ module.exports = {
     execute(message, args){
         if(message.guild.member(message.author).id !== '484644637420552202') {return message.reply("Incorrect userid.")}
 
-        let realmlist = ['shadow', 'earth', 'sky', 'hell', 'heaven', 'ground']
-  
-
+        let realmlist = ['shadow']
+        const realm = realmlist[Math.floor(Math.random() * array.length)];
+        if(realm === 'shadow'){
+            message.channel.send("test")
+        }
+/*
         let role = message.guild.roles.cache.find(r => r.id === "727541187883565076");
         let member = message.mentions.members.first();
         member.roles.remove(role)
-
+*/
 
       
     }
