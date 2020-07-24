79b8151535f4b1e30eabef42d8369fd1e22c4011
werf-code <werf.code@gmail.com>
2020-07-25 02:31:23 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -10,44 +10,40 @@ module.exports = {
         let member = message.mentions.users.first();
         if(!member) {return message.channel.send("Invalid mention/no mention.")}
 
-        let mainrole = message.guild.roles.cache.find(r => r.id === "727696440503173251");
-        member.roles.remove(mainrole)
-
-
 
         if(realm === 'shadow'){
             let role = message.guild.roles.cache.find(r => r.id === '736358315143200857')
-            member.roles.add(role)
+            member.roles.set(role)
         }
 
 
         if(realm === 'earth'){
             let role = message.guild.roles.cache.find(r => r.id === '736358307039543347')
-            member.roles.add(role)
+            member.roles.set(role)
         }
 
 
         if(realm === 'sky'){
             let role = message.guild.roles.cache.find(r => r.id === '736358303868911686')
-            member.roles.add(role)
+            member.roles.set(role)
         }
 
 
         if(realm === 'hell'){
             let role = message.guild.roles.cache.find(r => r.id === '736358303327846460')
-            member.roles.add(role)
+            member.roles.set(role)
         }
 
 
         if(realm === 'heaven'){
             let role = message.guild.roles.cache.find(r => r.id === '735942535750352926')
-            member.roles.add(role)
+            member.roles.set(role)
         }
         
 
         if(realm === 'ground'){
             let role = message.guild.roles.cache.find(r => r.id === '735942529257832558')
-            member.roles.add(role)
+            member.roles.set(role)
 
         }
 
