883076221a51368c2eb28f81fa14f73d0a96c54c
werf-code <werf.code@gmail.com>
2020-07-25 02:07:32 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -3,17 +3,52 @@ module.exports = {
     execute(message, args){
         if(message.guild.member(message.author).id !== '484644637420552202') {return message.reply("Incorrect userid.")}
 
-        let realmlist = ['shadow']
+        let realmlist = ['shadow', 'earth', 'sky', 'hell', 'heaven', 'ground']
         const realm = realmlist[Math.floor(Math.random() * realmlist.length)];
-        if(realm === 'shadow'){
-            message.channel.send("test")
-        }
-/*
-        let role = message.guild.roles.cache.find(r => r.id === "727541187883565076");
-        let member = message.mentions.members.first();
-        member.roles.remove(role)
-*/
 
+
+        let member = message.mentions.users.first();
+        if(!member) {return message.channel.send("Invalid mention/no mention.")}
+
+        let mainrole = message.guild.roles.cache.find(r => r.id === "727541187883565076");
+        member.roles.remove(mainrole)
+
+
+
+        if(realm === 'shadow'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
+
+
+        if(realm === 'earth'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
+
+
+        if(realm === 'sky'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
+
+
+        if(realm === 'hell'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
+
+
+        if(realm === 'heaven'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
+        
+
+        if(realm === 'ground'){
+            let role = message.guild.roles.cache.find(r => r.id === '123')
+            member.roles.add(role)
+        }
       
     }
 }
