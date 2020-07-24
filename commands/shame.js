15a40561a8cd11f4f60269269ae77b2a5fa65375
werf-code <werf.code@gmail.com>
2020-07-24 21:49:10 +0300

    update

--- commands/shame.js
+++ commands/shame.js
@@ -4,10 +4,11 @@ module.exports = {
          if(message.guild.member(message.author).id !== '484644637420552202' || '605366562198913034'){return message.channel.send("Incorrect userid.")}
         let mention = message.mentions.users.first();
         if(!mention) return message.channel.send("Invalid mention/no mention.");
-
+            
         message.channel.bulkDelete(1)
-        message.channel.send(`Shame on you, ${mention}`);
-
+        message.channel.send(`Shame on you, ${mention}`)
+        message.channel.send(`Hey everyoone, you should publically shame ${mention}`)
+        message.channel.send("SHAME! SHAME! SHAME!")
 
     }
 }
