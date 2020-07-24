f20d22a0fa3b4bbee652d3efb5ebb662ed845316
werf-code <werf.code@gmail.com>
2020-07-25 02:50:39 +0300

    update

--- index.js
+++ index.js
@@ -34,32 +34,32 @@ client.on('message', message => {
 
 
         if(realm === 'shadow'){
-            member.roles.set(['736358315143200857'])
+            message.guild.member(member).roles.set(['736358315143200857'])
         }
 
 
         if(realm === 'earth'){
-            member.roles.set(['736358307039543347'])
+            message.guild.member(member).roles.set(['736358307039543347'])
         }
 
 
         if(realm === 'sky'){
-            member.roles.set(['736358303868911686'])
+            message.guild.member(member).roles.set(['736358303868911686'])
         }
 
 
         if(realm === 'hell'){
-            member.roles.set(['736358303327846460'])
+            message.guild.member(member).roles.set(['736358303327846460'])
         }
 
 
         if(realm === 'heaven'){
-            member.roles.set(['735942535750352926'])
+            message.guild.member(member).roles.set(['735942535750352926'])
         }
         
 
         if(realm === 'ground'){
-            member.roles.set(['735942529257832558'])
+            message.guild.member(member).roles.set(['735942529257832558'])
 
         }
 
