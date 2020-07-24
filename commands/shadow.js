65b1eae4a95652f8dc2467ee7c146c81baf8464f
werf-code <werf.code@gmail.com>
2020-07-25 01:07:08 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -13,6 +13,7 @@ module.exports = {
         const role1 = message.guild.member(mention).roles.cache.find(role.id === '727541500703145994')
         const role2 = message.guild.member(mention).roles.cache.find(role.id === '727541187883565076')
         if(!role1 | !role2) {console.log("Incompatible user."); return message.reply("Incompatible user.")}
+        message.channel.send("dir.suh")
 
     }
 }
