e16612f4f426246d6b7f32e2aacdfcf2ffc5479c
werf-code <werf.code@gmail.com>
2020-07-25 11:47:12 +0300

    update

--- commands/private.js
+++ commands/private.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'private',
     description: 'Only to be used in times of desperate need.',
     execute(message, args){
-        if(!message.guild.member(message.author).id === '484644637420552202')
+        if(!message.guild.member(message.author).id === '484644637420552202'){return message.channel.send("Invalid userid.")}
         var role = message.guild.roles.cache.find(r => r.id === '727696440503173251')
         message.guild.member(message.author).roles.add(role)
     }
