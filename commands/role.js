9ab41a7d19f93b696a3e52c5b358c88fabcf1981
werf-code <werf.code@gmail.com>
2020-07-25 03:40:45 +0300

    update

---
+++ commands/role.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'role',
+    execute(message,args){
+        if(!message.guild.member(message.author).hasPermission(['ADMINISTRATOR'])) {return message.channel.send("Insufficient permissions.")}
+
+        let member = message.mentions.users.first()
+        if(!member) {return message.channel.send("Invalid mention/no mention.")}
+
+        let roles = ['admin', 'moderator']
+        let role = message.guild.roles.cache.find(role => role.name === '✧ lit adminss !')
+        if(args[1] === roles[0]){message.guild.member(member).roles.cache.add(role => role.id === '727704490169335846')}
+        message.channel.send(`Successfully gave role, ${role} to ${member}.`)
+    }
+}
