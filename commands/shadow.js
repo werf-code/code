ef8cc9ecbe35a3c00ea9a103935638d42b276569
werf-code <werf.code@gmail.com>
2020-07-25 03:24:08 +0300

    update

--- commands/shadow.js
+++ commands/shadow.js
@@ -1,6 +1,48 @@
 module.exports = {
     name: 'banish',
     execute(message, args){
-        return
+        if(!message.guild.member(message.author).roles.find(r => r.id === '736370787547742229')){
+            return message.reply("Insufficient permissions.")
+        } 
+        let realmlist = ['shadow', 'earth', 'sky', 'hell', 'heaven', 'ground']
+        const realm = realmlist[Math.floor(Math.random() * realmlist.length)];
+
+
+        let member = message.mentions.users.first();
+        if(!member) {return message.channel.send("Invalid mention/no mention.")}
+
+
+        if(realm === 'shadow'){
+            message.guild.member(member).roles.set(['736358315143200857'])
+        }
+
+
+        if(realm === 'earth'){
+            message.guild.member(member).roles.set(['736358307039543347'])
+        }
+
+
+        if(realm === 'sky'){
+            message.guild.member(member).roles.set(['736358303868911686'])
+        }
+
+
+        if(realm === 'hell'){
+            message.guild.member(member).roles.set(['736358303327846460'])
+        }
+
+
+        if(realm === 'heaven'){
+            message.guild.member(member).roles.set(['735942535750352926'])
+        }
+        
+
+        if(realm === 'ground'){
+            message.guild.member(member).roles.set(['735942529257832558'])
+
+        }
+
+        message.channel.send("Success")
+	  
     }
 }
--- index.js
+++ index.js
@@ -18,55 +18,6 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if(!message.content.startsWith(".banish")){
-		return console.log("No")
-	}
-
-	if(message.guild.member(message.author).id !== '484644637420552202') {return message.reply("Incorrect userid.")}
-
-        let realmlist = ['shadow', 'earth', 'sky', 'hell', 'heaven', 'ground']
-        const realm = realmlist[Math.floor(Math.random() * realmlist.length)];
-
-
-        let member = message.mentions.users.first();
-        if(!member) {return message.channel.send("Invalid mention/no mention.")}
-
-
-        if(realm === 'shadow'){
-            message.guild.member(member).roles.set(['736358315143200857'])
-        }
-
-
-        if(realm === 'earth'){
-            message.guild.member(member).roles.set(['736358307039543347'])
-        }
-
-
-        if(realm === 'sky'){
-            message.guild.member(member).roles.set(['736358303868911686'])
-        }
-
-
-        if(realm === 'hell'){
-            message.guild.member(member).roles.set(['736358303327846460'])
-        }
-
-
-        if(realm === 'heaven'){
-            message.guild.member(member).roles.set(['735942535750352926'])
-        }
-        
-
-        if(realm === 'ground'){
-            message.guild.member(member).roles.set(['735942529257832558'])
-
-        }
-
-        message.channel.send("Success")
-	  
-		
-})
 
 client.on('guildMemberAdd', (guildMember) => {
 	if(!guildMember.guild.id === '727471489527578665'){
