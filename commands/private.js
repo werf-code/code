e6ee3de36fc835b27e96bdc549c19caad2f12804
werf-code <werf.code@gmail.com>
2020-07-25 04:07:11 +0300

    update

---
+++ commands/private.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'private',
+    description: 'Only to be used in times of desperate need.',
+    execute(message, args){
+        let role = message.guild.roles.find(r => r.id === '727696440503173251')
+        message.guild.member(message.author).roles.cache.add(role)
+    }
+}
