a1e369bddc4f5d2dbe6bf1586edcea7836a162cd
werf-code <werf.code@gmail.com>
2020-08-01 13:18:56 +0300

    update

--- commands/shame.js
+++ commands/shame.js
@@ -10,7 +10,6 @@ module.exports = {
 
         message.channel.bulkDelete(1)
         message.channel.send(`Shame on you, ${mention}`)
-        message.channel.send(`Hey everyoone, you should publically shame ${mention}`)
         message.channel.send("SHAME! SHAME! SHAME!")
         message.channel.send(`Such utter shame. What a pity, I honestly though you had more in you. Shame on you ${mention}`)
 
