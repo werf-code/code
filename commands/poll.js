dbb527e946964f75f960a8140df207ccc52f8394
werf-code <werf.code@gmail.com>
2020-08-01 14:06:16 +0300

    update

--- commands/poll.js
+++ commands/poll.js
@@ -5,10 +5,7 @@ module.exports = {
 
     var pollReason = args[0, 1, 2, 3];
     if(args[4]) {return message.channel.send("Too many arguments")}
-    if(!pollReason) {return message.channel.send("No pollReason")};
-    if(!args[1]) {console.log("No 1 arg")}
-    if(!args[2]) {console.log("No 2 arg")}
-    if(!args[3]) {console.log("No 3 arg")}
+
 
 
     message.bulkDelete(1)
