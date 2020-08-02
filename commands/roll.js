a4314ead8546b16de071fde977df9cb503747e9f
werf-code <werf.code@gmail.com>
2020-08-02 14:51:36 +0300

    update

---
+++ commands/roll.js
@@ -0,0 +1,42 @@
+module.exports = {
+  name: 'roll.js',
+  description: 'rolls a predetermined 6, 20 or 40 sided die',
+  execute(message, args){
+
+    //Command Begins here.
+    const Discord = require('discord.js');
+    const embed = new Discord.MessageEmbed();
+    let size = ['6', '20', 'max']
+
+//Check if the author's administrator, if true, inform of max dice.
+    if(message.guild.member(message.author).hasPermissions('ADMINISTRATOR')){
+      message.channel.send("Because you have administrative privilages, you can active the 40 sided die. Usage: '-roll max'");
+    }else {let size = ['6', '20']}
+
+//If there's no args, send proper usage to chat.
+    if(!args[0]) {
+      .setTitle("Proper Usage: -roll {6, 20, max(requires administrative privilages)}");
+      return message.channel.send(embed)
+    };
+
+//6 sided die command.
+    if(args[0] = size[0]){
+      let sides ['1', '2', '3', '4', '5', '6'];
+    };
+
+    if(args[0] = size[1]){
+      let sides ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+    };
+
+//Chooses result and sets it as the title.
+    var result = sides[Math.floor(Math.random() * sides.length)]
+    .setTitle(`And the result is, ${result}`)
+
+//Sends command to chat.
+    .setColor("#8403fc")
+    .setFooter("Clark, hosted by heroku.")
+    message.channel.send(embed)
+
+
+  }
+}
